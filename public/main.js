ebe541ee3b52252fa5c3616c86b1c87c0b4246ac
youweulin <metaworldfood@gmail.com>
2026-05-07 14:24:49 +0800

    fix: configure TURN server to fix cross-network voice streaming

--- public/main.js
+++ public/main.js
@@ -85,6 +85,21 @@ const state = {
   ICE_SERVERS: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
   ],
 };
 
